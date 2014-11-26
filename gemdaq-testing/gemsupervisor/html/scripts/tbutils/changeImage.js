ee1c5dbf3ce9821a54474e5a7e88117fe16dcfd8
Sergey Baranov <sergey.baranov@cern.ch>
2014-11-26 14:28:26 +0100

    images path ahs been updated

--- gemdaq-testing/gemsupervisor/html/scripts/tbutils/changeImage.js
+++ gemdaq-testing/gemsupervisor/html/scripts/tbutils/changeImage.js
@@ -2,6 +2,6 @@ $(document).ready( function () {
 	function changeImage (form) {
 	    var histNum = form.ChannelHist.value;
 	    alert("Selecting channel "+histNum+" histogram");
-	    document.getElementById("vfatChannelHisto").src="/gemdaq/gemsupervisor/html/images/tbutils/tscan/channel"+histNum+".png";
+	    document.getElementById("vfatChannelHisto").src="/gemdaq/gemsupervisor/baranov/images/tbutils/tscan/channel"+histNum+".png";
 	});
     });
