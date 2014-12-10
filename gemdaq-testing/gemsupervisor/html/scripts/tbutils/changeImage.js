85f27baa7ccf58ce09c2fd9dc069b73628ca93b2
Sergey Baranov <sergey.baranov@cern.ch>
2014-12-10 16:32:55 +0100

    cleaning

--- gemdaq-testing/gemsupervisor/html/scripts/tbutils/changeImage.js
+++
@@ -1,8 +0,0 @@
-$(document).ready( function () {
-    });
-
-function changeImage (form) {
-    var histNum = form.ChannelHist.value;
-    //alert("Selecting channel "+histNum+" histogram");
-    document.getElementById("vfatChannelHisto").src="/gemdaq/gemsupervisor/html/images/tbutils/tscan/chanthresh"+histNum+".png";
-};
--- glib-testing/compile.sh
+++
@@ -1,9 +0,0 @@
-mkdir build 
-cd build
-cmake ..
-make clean
-echo ${CXXFLAGS}
-make CXXFLAGS="-O0 -g3 -fno-inline"
-echo "Deleting Temporary File created during compilation"
-cd ..
-rm -rf build 
