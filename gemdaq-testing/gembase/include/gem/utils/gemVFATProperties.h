39fb1b2ed0f5627abe9795aea58ee42547961fc3
Mykhailo Dalchenko <mykhailo.dalchenko@cern.ch>
2015-02-16 21:41:55 +0100

    Adding device properties classes

---
+++ gemdaq-testing/gembase/include/gem/utils/gemVFATProperties.h
@@ -0,0 +1,25 @@
+/*
+ * This class provides interface for the VFAT properties.
+ */
+#ifndef gem_base_utils_gemVFATProperties_h
+#define gem_base_utils_gemVFATProperties_h
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include "gemDeviceProperties.h"
+
+namespace gem {
+    namespace base {
+        namespace utils {
+            class gemVFATProperties public gemDeviceProperties {
+                friend class gem::base::utils::gemXMLparser;
+                gemVFATProperties(){}
+                ~gemVFATProperties(){}
+            }
+        }
+    }
+}
+
+#endif
