31277ff69822aadf9d01d613e9f1d69c17142e8b
Mykhailo Dalchenko <mykhailo.dalchenko@cern.ch>
2015-02-24 17:06:13 -0600

    Changing class structure, implementing better inheritance and polymorphism

--- gemdaq-testing/gemHwMonitor/src/common/gemCrateMonitorBase.cc
+++ gemdaq-testing/gemHwMonitor/src/common/gemCrateMonitorBase.cc
@@ -1,8 +1,5 @@
 #include "gem/hwMonitor/gemCrateMonitorBase.h"
 
-//XDAQ_INSTANTIATOR_IMPL(gem::hwMonitor::gemHwMonitorBase)
-
-//gem::hwMonitor::gemHwMonitorBase::gemHwMonitorBase(xdaq::ApplicationStub * s)
 gem::hwMonitor::gemHwMonitorBase::gemHwMonitorBase()
     throw (xdaq::exception::Exception)
 {
