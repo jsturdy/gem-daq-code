399a3e59d71a0366cde36feefca836d7cc112cdb
Jared Sturdy <sturdy@cern.ch>
2015-08-04 09:27:13 +0200

    formatting tab treated as three characters, starting to apply to all files

--- gemdaq-testing/gemHwMonitor/include/gem/hwMonitor/gemHwMonitorHelper.h
+++ gemdaq-testing/gemHwMonitor/include/gem/hwMonitor/gemHwMonitorHelper.h
@@ -12,58 +12,58 @@
 #include "gemHwMonitorBase.h"
 
 namespace gem {
-    namespace hwMonitor {
-        class gemHwMonitorHelper
-        {
-            public:
-                gemHwMonitorHelper(gemHwMonitorSystem* gemSystem)
-                    throw (xdaq::exception::Exception)
-                {
-                    std::string defaulXMLcfgFile = std::getenv("BUILD_HOME");
-                    defaulXMLcfgFile +="/gemdaq-testing/gembase/xml/gem_test_904.xml";
-                    this->setXMLconfigFile(defaulXMLcfgFile.c_str());
-                    ptr_gemSystem_ = gemSystem;
-                    ptr_gemSystem_->setDeviceStatus(2);
-                }
+	namespace hwMonitor {
+		class gemHwMonitorHelper
+		{
+		public:
+			gemHwMonitorHelper(gemHwMonitorSystem* gemSystem)
+				throw (xdaq::exception::Exception)
+				{
+					std::string defaulXMLcfgFile = std::getenv("BUILD_HOME");
+					defaulXMLcfgFile +="/gemdaq-testing/gembase/xml/gem_test_904.xml";
+					this->setXMLconfigFile(defaulXMLcfgFile.c_str());
+					ptr_gemSystem_ = gemSystem;
+					ptr_gemSystem_->setDeviceStatus(2);
+				}
 
-                virtual ~gemHwMonitorHelper()
-                {
-                    delete gemXMLparser_;
-                }
+			virtual ~gemHwMonitorHelper()
+				{
+					delete gemXMLparser_;
+				}
 
-                void setXMLconfigFile (const char* inputXMLfilename)
-                    throw (xgi::exception::Exception)
-                {
-                    xmlConfigFileName_ = inputXMLfilename;
-                    if (ptr_gemSystem_) 
-                        ptr_gemSystem_->setIsConfigured(false);
-                    else 
-                        std::cout << "[WARNING] New XML file set, but device state hasn't changed (NULL-pointer)" << std::endl;
-                }
-                const std::string getXMLconfigFile ()
-                    throw (xgi::exception::Exception)
-                {
-                    return xmlConfigFileName_;
-                }
-                void setDBSconfigFile (std::string inputDBSfilename)
-                    throw (xgi::exception::Exception) {}
-                const std::string getDBSconfigFile ()
-                    throw (xgi::exception::Exception) { return "Not implemented yet"; }
-                void configure()
-                    throw (xgi::exception::Exception)
-                {
-                    gemXMLparser_ = new gem::utils::gemXMLparser(xmlConfigFileName_);
-                    gemXMLparser_->parseXMLFile();
-                    ptr_gemSystem_->setDeviceConfiguration(*(gemXMLparser_->getGEMDevice()));
-                }
+			void setXMLconfigFile (const char* inputXMLfilename)
+				throw (xgi::exception::Exception)
+			{
+				xmlConfigFileName_ = inputXMLfilename;
+				if (ptr_gemSystem_) 
+					ptr_gemSystem_->setIsConfigured(false);
+				else 
+					std::cout << "[WARNING] New XML file set, but device state hasn't changed (NULL-pointer)" << std::endl;
+			}
+			const std::string getXMLconfigFile ()
+				throw (xgi::exception::Exception)
+			{
+				return xmlConfigFileName_;
+			}
+			void setDBSconfigFile (std::string inputDBSfilename)
+				throw (xgi::exception::Exception) {}
+			const std::string getDBSconfigFile ()
+				throw (xgi::exception::Exception) { return "Not implemented yet"; }
+			void configure()
+				throw (xgi::exception::Exception)
+			{
+				gemXMLparser_ = new gem::utils::gemXMLparser(xmlConfigFileName_);
+				gemXMLparser_->parseXMLFile();
+				ptr_gemSystem_->setDeviceConfiguration(*(gemXMLparser_->getGEMDevice()));
+			}
 
-            protected:
-            private:
-                gemHwMonitorSystem* ptr_gemSystem_;
-                gem::utils::gemXMLparser* gemXMLparser_;
-                std::string xmlConfigFileName_;
+		protected:
+		private:
+			gemHwMonitorSystem* ptr_gemSystem_;
+			gem::utils::gemXMLparser* gemXMLparser_;
+			std::string xmlConfigFileName_;
 
-        };
-    }
+		};
+	}
 }
 #endif
