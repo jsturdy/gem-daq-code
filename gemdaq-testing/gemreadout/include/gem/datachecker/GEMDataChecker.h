d9d4c7ebba7580aecb53360a786d5d66b0c093fe
mexanick <mykhailo.dalchenko@cern.ch>
2015-08-13 19:14:26 -0500

    Migrating dataChecker

--- gemdaq-testing/gemreadout/include/gem/datachecker/GEMDataChecker.h
+++ gemdaq-testing/gemreadout/include/gem/datachecker/GEMDataChecker.h
@@ -2,30 +2,35 @@
 #define gem_datachecker_GEMDataChecker_h
 
 //#include "gem/utils/GEMLogging.h"
+#include <stdint.h>
 
 namespace gem {
-  namespace readout {
-    struct VFATData;
-    struct GEBData;
-    struct GEMData;
-  }
+//  namespace readout {
+//    struct VFATData;
+//    struct GEBData;
+//    struct GEMData;
+//  }
   namespace datachecker {
     class GEMDataChecker {
     public:
 
-      GEMDataChecker(gem::readout::GEMData& gem, gem::readout::GEBData& geb, 
-                     gem::readout::VFATData& vfat);
+      GEMDataChecker(){}
+      //GEMDataChecker(gem::readout::GEMData& gem, gem::readout::GEBData& geb, 
+      //               gem::readout::VFATData& vfat);
       ~GEMDataChecker() {};
 
-      int  counterGEMdata  (gem::readout::GEMData& gem, gem::readout::GEBData& geb, 
-                            gem::readout::VFATData& vfat);
+      //int  counterGEMdata  (gem::readout::GEMData& gem, gem::readout::GEBData& geb, 
+      //                      gem::readout::VFATData& vfat);
 
+      uint16_t checkCRC(uint16_t dataVFAT[11], bool OKprint);
+      
     private:
 
       // log4cplus::Logger gemLogger_;
       int event_counter_;
-
-    };
-  }
+      uint16_t crc_calc(uint16_t crc_in, uint16_t dato);
+      
+      };
+   }
 }
 #endif
