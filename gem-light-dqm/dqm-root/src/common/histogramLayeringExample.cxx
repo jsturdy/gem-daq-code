5e2b8cf3a4d99998934906bf65f01bc1d938f473
kingr <robertdkingjr@gmail.com>
2015-08-19 13:57:37 -0500

    Added plotAll to save individual histograms easily from GGSW test runs

--- gem-light-dqm/dqm-root/src/common/histogramLayeringExample.cxx
+++ gem-light-dqm/dqm-root/src/common/histogramLayeringExample.cxx
@@ -34,6 +34,7 @@
 #include <TTree.h>
 #include <TBranch.h>
 #include <time.h>
+#include <math.h>
 
 #include "dataChecker.cc"
 #include "plotter.cxx"
@@ -57,20 +58,14 @@ int main(int argc, char** argv)
     //Create output file in proper location
     time_t t = time(NULL);
     tm* timePtr = localtime(&t);
-    char oPath [32];
+    char oPath [64];
     char oName [64];
-    char oCommand [32];
-    strftime (oPath, 32, "./output/%m_%d/", timePtr);
-    strftime (oName, 64, "./output/%m_%d/%b%d_%G-%H_%M.root", timePtr);
-    strftime (oCommand, 32, ".!mkdir -p ./output/%m_%d/", timePtr);
-    
-    cout << oPath << endl;
-    cout << oName << endl;
-    cout << oCommand << endl;
-
-    //return 0;
+    char oCommand [64];
+    strftime (oPath, 64, "./output/%m_%d-%H/%b%d_%G-%H_%M_%S/", timePtr);
+    strftime (oName, 64, "./output/%m_%d-%H/%b%d_%G-%H_%M_%S/%b%d_%G-%H_%M_%S.root", timePtr);
+    strftime (oCommand, 64, ".!mkdir -p ./output/%m_%d-%H/%b%d_%G-%H_%M_%S/", timePtr);
    
-
+   
     vector<vector<TH1*>> hs;
   
    
@@ -88,10 +83,6 @@ int main(int argc, char** argv)
 		continue;
 	    if(line.substr(line.length()-5,5)!=".root")
 	    {		
-		cout << "########################"<< endl;
-		cout << "Detected incorrect file: " << line << endl;
-		cout << "########################"<< endl;
-		cout << endl;
 		cout << "Input Files must be in .root format." << endl;
 		cout << endl;
 		return 0;
@@ -100,7 +91,7 @@ int main(int argc, char** argv)
 
 	    ifilenames.push_back(line);
 	    ifiles.push_back(new TFile(ifilenames[numF-1], "READ"));
-	    cout<<"Input File "<<numF<<": "<<line<<endl;
+	    cout<<"Input File "<<numF+1<<": "<<line<<endl;
 	      
 	}
     	textfile.close();
@@ -111,39 +102,39 @@ int main(int argc, char** argv)
     	return 0;
     }
     
-    if (numF < 2)
+    if (numF < 1)
     {
-	cout << "Number of files must be at least 2." << endl;
+	cout << "Number of files must be at least 1." << endl;
 	return 0;
     }
-    
+
+    vector<TString> inames; //name of file without extension
+    for (int i=0;i<ifilenames.size();i++)
+    {
+	TString name = ifilenames[i];
+	int counter=0;
+	int loc = name.Last('/')+1;
+	int len = name.Length()-loc-5;
+	TSubString subname = name(loc,len);
+	inames.push_back(subname);
+    }
+
+
     gROOT->ProcessLine(oCommand);
     TFile *ofile = new TFile(oName, "RECREATE");
     cout<<"Output File: "<< oName <<endl<<endl;
 
     int counter;
-    // vector<TList*> keylists;
-    // vector<TIter> nexts;
-    // vector<TKey*> keys;
-
-    //TIter next;
     vector<TH1*> tempH;
 
+    //Retrieve histograms from all files
     for (int i=0;i<numF;i++)
     {
-	// keylists.push_back(new TList* [ifiles[i]->GetListOfKeys()]);
-	// cout << endl << "List of keys in Input File " << i << ":" << endl;
-	// keylists[i]->Print();
-	// cout << "begin loops" << endl;
-	// nexts.push_back(new TIter* [keylists[i]]);
-	// keys.push_back(new TKey*);
-	// counters.push_back(new int[0]);
-
 	TList* keylist = new TList;
 	keylist = ifiles[i]->GetListOfKeys();
-	cout << endl << "List of keys in Input File " << i << ":" << endl;
-	keylist->Print();
-	cout << "==Begin Loops==" << endl;
+	//cout << endl << "List of keys in Input File " << i << ":" << endl;
+	//keylist->Print();
+	//cout << "==Begin Loops==" << endl;
 	
 	TIter nextkey(keylist);
 	TKey *key = new TKey;
@@ -152,21 +143,20 @@ int main(int argc, char** argv)
 	while (key = (TKey*)nextkey())
 	{
 	    counter++;
-	    cout << "Loop through file "<<i<<": "<< counter;
+	    //cout << "Loop through file "<<i<<": "<< counter;
 	    TClass *cl = gROOT->GetClass(key->GetClassName());
 	    if (!cl->InheritsFrom("TH1")) 
 	    {
-		cout << endl;
+		//cout << endl;
 		continue;
 	    }
-	    cout << " - found " << key->GetClassName() << endl;
+	    //cout << " - found " << key->GetClassName() << endl;
 	    TH1 *h = (TH1*)key->ReadObj();
 	    tempH.push_back(h);
 	}
 
 	hs.push_back(tempH);
 	tempH.clear();
-
     }
 
     //Make sure all files have same number of histograms
@@ -183,70 +173,14 @@ int main(int argc, char** argv)
     }
     int numH = hs[0].size(); //number of histograms in each file
 
-    // vector<TH1*> hist1; 
-    // vector<TH1*> hist2;
-    // int cnt1=0; //counts iterations through ifile1
-    // int cnt2=0; //counts iterations through ifile2
 
-    // //List all keys
-    // TList *keylist1 = ifiles[0]->GetListOfKeys();
-    // cout << endl << "List of keys in Input File 1:" << endl;
-    // keylist1->Print();
-    // TList *keylist2 = ifiles[1]->GetListOfKeys();
-    // cout << endl << "List of keys in Input File 2:" << endl;
-    // keylist2->Print();
-    // cout << endl;
 
-    // cout << "begin loops" << endl;
-    // TIter next1(keylist1);
-    // TKey *key1;
-    // while ((key1 = (TKey*)next1())) 
-    // {
-    // 	cnt1++;
-    // 	cout << "loop through file1: " << cnt1;
-    // 	TClass *cl = gROOT->GetClass(key1->GetClassName());
-    //     if (!cl->InheritsFrom("TH1"))
-    // 	{
-    // 	    cout << endl;
-    // 	    continue;
-    // 	}
-    // 	cout << " - found " << key1->GetClassName() << endl;
-    //     TH1 *h = (TH1*)key1->ReadObj();
-    // 	hist1.push_back(h);
-    // }
-    // cout << "----------" << endl;
-    // TIter next2(keylist2);
-    // TKey *key2;
-    // while ((key2 = (TKey*)next2())) 
-    // {
-    // 	cnt2++;
-    // 	cout << "loop through file2: " << cnt2;
-    // 	TClass *cl = gROOT->GetClass(key2->GetClassName());
-    //     if (!cl->InheritsFrom("TH1"))
-    // 	{ 
-    // 	    cout<<endl;
-    // 	    continue;
-    // 	}
-    // 	cout << " - found " << key2->GetClassName() << endl;
-    //     TH1 *h = (TH1*)key2->ReadObj();
-    // 	hist2.push_back(h);
-    // }
-    // cout << "end loops" << endl;
-    // cout << endl;
-    // cout << "hist1 size: " << hist1.size() << endl;
-    // cout << "hist2 size: " << hist2.size() << endl;
+    //Plot single canvas with all histograms layered
+    layerAll(hs, inames);
 
-    // if(hist1.size() != hist2.size())
-    // {
-    // 	cout<<"Error: differing number of histograms."<<endl;
-    // 	return 0;
-    // }
-    
 
-    
-    
 
-    vector<TH1*> hists_ready; //holds same individual histogram from each file - ready to be passed to plotter
+    vector<TH1*> hists_ready; //holds similar histogram from each file - ready to be passed to plotter
 
     //Layer Histograms, print/save individually
     char layerAnyways = 'n';
@@ -270,37 +204,20 @@ int main(int argc, char** argv)
 	    hists_ready.push_back(hs[k][i]);
 
      
-	TCanvas *c = newCanvas();
-	c->SetTitle(hs[0][i]->GetName());
-	c->SetName(hs[0][i]->GetName());
+	TCanvas *mainc = newCanvas();
+	mainc->Divide(1,1);
+	const char* cname = hs[0][i]->GetName();
+	//char* fcname;
+	//strcat(fcname, cname);
+	mainc->SetTitle(cname);
+	mainc->SetName(cname);
 	    
-	layerHistogram(hists_ready, c);
-	c->Write();
-
-    // 	gROOT->ProcessLine(".!mkdir -p ./output/"+outday+"/"+outname+"/");
-    // 	c->Print( "output/"+outday+"/"+outname+"/"+class1+".pdf","pdf");
-    // 	c->Print( "output/"+outday+"/"+outname+"/"+class1+".root","root");
+	layerHistogram(hists_ready,inames,mainc,1);
+	mainc->Write();
+    
     }
 
     
-
-
-
-    // //Better method for layering
-    // //Layers all histograms onto same canvas, prints/saves
-    // TCanvas *c = newCanvas();
-    // c->Divide(3,4);
-    // for (int i=0;i<hist1.size();i++)
-    // {
-    // 	layerHistogram(hist1[i], hist2[i], c, i+1);
-    // }
-    // cout<<endl<<"Printing all Histograms."<<endl;
-    // c->Write();
-    // // gROOT->ProcessLine(".!mkdir -p ./output/"+outday+"/"+outname+"/");
-    // // c->Print( "output/"+outday+"/"+outname+"/"+"histograms.pdf","pdf");
-    // // c->Print( "output/"+outday+"/"+outname+"/"+"histograms.root","root");
-
-     
 
     //CRC Canvas
     cout << endl << "Drawing CRC Canvas" << endl;
@@ -310,9 +227,9 @@ int main(int argc, char** argv)
     crc->Divide(2,1);
         
     vector<const char*> crc_names;
-    crc_names.push_back("CRC");
+    crc_names.push_back("CRC1_vs_CRC2");
     crc_names.push_back("DiffCRC");  
-    layerSpecific(hs, crc_names, crc); 
+    layerSpecific(hs,crc_names,inames,crc); 
 
 
 
@@ -331,7 +248,36 @@ int main(int argc, char** argv)
     cbf_names.push_back("1100");
     cbf_names.push_back("1110");
     cbf_names.push_back("Flag");
-    layerSpecific(hs, cbf_names, cbf);
+    layerSpecific(hs,cbf_names,inames,cbf);
+
+
+
+    //GGSW check
+    cout << "Drawing GGSW Check Canvas" << endl;
+    TCanvas *ggsw = newCanvas();
+    ggsw->SetTitle("GGSW Check");
+    ggsw->SetName("GGSW Check");
+    ggsw->Divide(2,1);
+    vector<const char*> ggsw_names;
+    ggsw_names.push_back("VFAT");
+    ggsw_names.push_back("ChipID");
+    layerSpecific(hs,ggsw_names,inames,ggsw);
+
+
+
+    //Print canvases as other types
+    TString pdf = oPath;
+    pdf+="pdf/";
+    TString png = oPath;
+    png+="png/";
+    TString jpg = oPath;
+    jpg+="jpg/";
+    TString root = oPath;
+    root+="root/";
+    printCanvases("pdf", pdf);
+    printCanvases("png", png);
+    printCanvases("jpg", jpg);
+    //printCanvases("root", root);
 
 
 
--- gem-light-dqm/dqm-root/src/common/plotter.cxx
+++ gem-light-dqm/dqm-root/src/common/plotter.cxx
@@ -30,6 +30,7 @@
 #include "TH1F.h"
 #include <TFile.h>
 #include "TPaveStats.h"
+#include <math.h>
 
 #include <iostream>
 
@@ -303,6 +304,23 @@ void printCanvases(TString type)
     }
 }
 
+
+void printCanvases(TString type, TString prefix="")
+{
+    
+    TIter next(gROOT->GetListOfCanvases());
+    TCanvas *c;
+    while ((c = (TCanvas *)next()) )
+    {
+	TString name =  c->GetTitle();
+        if (prefix!="") gROOT->ProcessLine(".!mkdir -p ./"+prefix);
+        c->Print(prefix+name+"."+type,type);
+    }
+}
+
+
+
+
 // Set histogram titles
 void setTitles(TH1 *h, TString xtitle, TString ytitle)
 {
@@ -338,193 +356,156 @@ void printHistograms(TString type, TString prefix="")
     }
 }
 
-//Print single layered histogram into separate files located in prefix
-//ORIGINAL-OUTDATED, USE OTHER METHOD
-void layerHistogram(TH1 *h1, TH1 *h2, TString prefix="")
-{
-    TCanvas *c = newCanvas();
- 
-    //Coloring and Styling
-    h1->SetMarkerColor(kBlue);
-    h1->SetMarkerStyle(7);
-    h1->SetFillStyle(3005);
-    h1->SetFillColor(kBlue);
-    h1->SetLineColor(kBlue);
-
-    h2->SetMarkerColor(kRed);
-    h2->SetMarkerStyle(7);
-    h2->SetFillStyle(3004);
-    h2->SetFillColor(kRed);
-    h2->SetLineColor(kRed);
-
-    //Sizing
-    double_t max1 = h1->GetBinContent(h1->GetMaximumBin());
-    double_t min1 = h1->GetBinContent(h1->GetMinimumBin());
-    double_t max2 = h2->GetBinContent(h2->GetMaximumBin());
-    double_t min2 = h2->GetBinContent(h2->GetMinimumBin());
-
-    cout << "max1: " << max1 << endl;
-    cout << "min1: " << min1 << endl;
-    cout << "max2: " << max1 << endl;
-    cout << "min2: " << min1 << endl;
-    
-    if (max2 > max1)
-	h1->SetMaximum(max2);
-    if (min2 < min1)
-	h1->SetMinimum(min2);
-
-    h1->Draw();
-    h2->Draw("same");
-
-    TString name =  h1->GetTitle();
-    if (prefix!="") gROOT->ProcessLine(".!mkdir -p ./"+prefix);
-    c->Print(prefix+name+".pdf","pdf");
-    c->Print(prefix+name+".root","root");
-}
-
-
-
-//Draw layered histogram of h1 and h2 onto specified canvas and pad number
-void layerHistogram(TH1 *h1, TH1 *h2, TCanvas *c, int pad=1)
-{
+//Prints each individual histogram to picture formats
+void printPictures(TH1 *h, TString iname, TString opath, TCanvas *c, int pad=1)
+{							
     c->cd(pad);
+    int max=h->GetBinContent(h->GetMaximumBin());
+    h->SetMaximum(max*1.1);
+    h->SetMinimum(0);
 
-    //Coloring and Styling
-    h1->SetMarkerColor(kBlue);
-    h1->SetMarkerStyle(7);
-    h1->SetFillStyle(3005);
-    h1->SetFillColor(kBlue);
-    h1->SetLineColor(kBlue);
+    //Focus on Valid ChipIDs
+    if (strcmp(h->GetName(),"ChipID")==0)
+	h->GetXaxis()->SetRangeUser(3700,3800);
 
-    h2->SetMarkerColor(kRed);
-    h2->SetMarkerStyle(7);
-    h2->SetFillStyle(3004);
-    h2->SetFillColor(kRed);
-    h2->SetLineColor(kRed);
+    //Set color/style
+    h->SetMarkerColor(kBlue+3);
+    h->SetMarkerStyle(1);
+    h->SetFillStyle(1001);
+    h->SetFillColor(kBlue+3);
+    h->SetLineColor(kBlue+3);
 
-    //Sizing
-    double_t max1 = h1->GetBinContent(h1->GetMaximumBin());
-    double_t min1 = h1->GetBinContent(h1->GetMinimumBin());
-    double_t max2 = h2->GetBinContent(h2->GetMaximumBin());
-    double_t min2 = h2->GetBinContent(h2->GetMinimumBin());
+    gStyle->SetOptStat(111111);
 
-    //cout << "max1: " << max1 << endl;
-    //cout << "min1: " << min1 << endl;
-    //cout << "max2: " << max2 << endl;
-    //cout << "min2: " << min2 << endl;
+    h->Draw();
+
+    //Add Legend
+    TLegend* leg = new TLegend(0.78,0.4,0.98,0.45);
+    leg->AddEntry(h,iname,"l");
+    leg->Draw();
+
+    gROOT->ProcessLine(".!mkdir -p "+opath+"/individual_pdfs/");
+    c->Print(opath+"individual_pdfs/"+iname+"--"+h->GetName()+".pdf","pdf");
+
+    gROOT->ProcessLine(".!mkdir -p "+opath+"/individual_pngs/");
+    c->Print(opath+"individual_pngs/"+iname+"--"+h->GetName()+".png","png");
+
+    gROOT->ProcessLine(".!mkdir -p "+opath+"/individual_jpgs/");
+    c->Print(opath+"individual_jpgs/"+iname+"--"+h->GetName()+".jpg","jpg");
     
-    if (max2 > max1)
-    {
-	//cout << "MAXIMUM SET TO "<<max2<<endl;
-	h1->SetMaximum(max2*1.1);
-    }
-    if (min2 < min1)
-    {
-	//cout << "MINIMUM SET TO "<<min2<<endl;
-	h1->SetMinimum(min2*1.1);
-    }
-
-    h1->Draw();
-    //h2->Draw("SAMES");
-    c->Update();
-
-    //Retrieve stat box
-    TPaveStats *st = (TPaveStats*)h1->FindObject("stats");
-    TList *list1 = st->GetListOfLines();
-    //list1->Print();
-    
-
-
-    //Color code title
-    TText *title = st->GetLine(0);
-    title->Print();
-    TText *newtitle = title;
-    list1->Remove(title);
-    newtitle->SetTextColor(kBlue);
-    newtitle->Print();
-    list1->AddFirst(newtitle);
-
-
-
-    //Reposition
-    double x1 = st->GetX1NDC();
-    double x2 = st->GetX2NDC();
-    cout<< st->GetX1NDC()<<endl;
-    cout<< st->GetX2NDC()<<endl;
-    double diff = x2-x1;
-    st->SetX1NDC(x1-(diff+.04));  //new x start position
-    st->SetX2NDC(x2-(diff+.04));  //new x end position
-    
-
-
-    h2->Draw("SAMES");
-    c->Update();
-  
-    //Retrieve stat box
-    TPaveStats *st2 = (TPaveStats*)h2->FindObject("stats");
-    TList *list2 = st2->GetListOfLines();
-    
-
-    //Color code title
-    st2->GetLine(0)->SetTextColor(kRed);
-    
+    gROOT->ProcessLine(".!mkdir -p "+opath+"/individual_roots/");
+    c->Print(opath+"individual_roots/"+iname+"--"+h->GetName()+".root","root");
 }
 
-//Allows for layering of more than 2 histograms using a vector
-void layerHistogram(vector<TH1*> h, TCanvas *c, int pad=1)
+//Layers all histograms in h on single canvas/pad in different colors/styles
+void layerHistogram(vector<TH1*> h, vector<TString> inames, TCanvas *c, int pad=1)
 {
-    int num_hist = h.size();
-    //cout << "Number of histograms: " << num_hist << endl;
+    int numH = h.size();
+    cout << "Number of histograms: " << numH << endl;
     vector<int> maxs;
-    int max=0;
-    vector<int> mins;
-    int min=0;
+    int max=h[0]->GetBinContent(h[0]->GetMaximumBin());
+    // vector<int> mins;
+    // int min=h[0]->GetBinContent(h[0]->GetMinimumBin());
+    h[0]->SetMaximum(max*1.1);
+    h[0]->SetMinimum(0);
 
+    //Focus on Valid ChipIDs
+    if (strcmp(h[0]->GetName(),"ChipID")==0)
+    {
+	for(int f=0;f<numH;f++)
+	{
+	    h[f]->GetXaxis()->SetRangeUser(3700,3800);
+	}
+    }
+	
     c->cd(pad);
-
-    for (int i=0;i<num_hist;i++)
+    int n = 0;
+    for (int i=0;i<numH;i++)
     { 
 	//Set color/style
-	if (num_hist==2) //Use red/blue when comparing only 2
+	
+	if (i==4) //avoid yellow
+	    n++; 
+	
+	if (numH==1)
 	{
-	    h[i]->SetMarkerColor(2*(1+i));
-	    h[i]->SetMarkerStyle(7);
-	    h[i]->SetFillStyle(i+3004);
-	    h[i]->SetFillColor(2*(1+i));
-	    h[i]->SetLineColor(2*(1+i));	 
+	    h[i]->SetMarkerColor(kBlue+3);
+	    h[i]->SetMarkerStyle(1);
+	    h[i]->SetFillStyle(1001);
+	    h[i]->SetFillColor(kBlue+3);
+	    h[i]->SetLineColor(kBlue+3);
 	}
-	else if (num_hist>9 and i>9) //Very unlikely to have more than 9 histograms, but it works
+	else if (numH>9 and i>9) //Should never have more than 9 histograms, but it works
 	{
-	    h[i]->SetMarkerColor(i+30);
-	    h[i]->SetMarkerStyle(7);
+	    h[i]->SetMarkerColor(i+30+n);
+	    h[i]->SetMarkerStyle(1);
 	    h[i]->SetFillStyle(i+3004);
-	    h[i]->SetFillColor(i+30);
-	    h[i]->SetLineColor(i+30);
+	    h[i]->SetFillColor(i+30+n);
+	    h[i]->SetLineColor(i+30+n);
+	}
+	else if (numH<5)
+	{
+
+	    h[i]->SetMarkerColor(pow(2,i));
+	    h[i]->SetMarkerStyle(1);
+	    h[i]->SetFillStyle(i+3004);
+	    h[i]->SetFillColor(pow(2,i));
+	    h[i]->SetLineColor(pow(2,i));
 	}
 	else
 	{
-	    h[i]->SetMarkerColor(i+2);
-	    h[i]->SetMarkerStyle(7);
+	    h[i]->SetMarkerColor(i+1+n);
+	    h[i]->SetMarkerStyle(1);
 	    h[i]->SetFillStyle(i+3004);
-	    h[i]->SetFillColor(i+2);
-	    h[i]->SetLineColor(i+2);
+	    h[i]->SetFillColor(i+1+n);
+	    h[i]->SetLineColor(i+1+n);
 	}
 
 	//Resize as necessary
 	maxs.push_back(h[i]->GetBinContent(h[i]->GetMaximumBin()));
-	mins.push_back(h[i]->GetBinContent(h[i]->GetMinimumBin()));
+	// mins.push_back(h[i]->GetBinContent(h[i]->GetMinimumBin()));
+
 	if (maxs[i]>max)
-	    h[0]->SetMaximum(maxs[i]);
+	    h[0]->SetMaximum(maxs[i]*1.1);
 	
-	if (mins[i]<min)
-	    h[0]->SetMinimum(mins[i]);
+	// if (mins[i]<min)
+	//     h[0]->SetMinimum(mins[i]*1.1);
     }
+
+    gStyle->SetOptStat(111111);
     h[0]->Draw();
-    for (int i=1;i<num_hist;i++)
+    for (int i=0;i<numH;i++)
 	    h[i]->Draw("SAMES");
+
+
+   //Add Legend
+    TLegend* leg = new TLegend(0.78,0.4,0.98,0.4+0.05*numH);
+    for (int l=0;l<numH;l++)
+	leg->AddEntry(h[l],inames[l],"l");
+    leg->Draw();
+
+
+
+    c->Update();
+
+
+    for (int f=0;f<numH;f++)
+    {	
+    	//Retrieve stat box
+    	TPaveStats *st = (TPaveStats*)h[f]->FindObject("stats");
+   
+    	//Color code
+	st->SetTextColor(pow(2,f));
+
+    	//Reposition
+    	st->SetX1NDC(.78-0.22*f);  //new x start position
+    	st->SetX2NDC(.98-0.22*f);  //new x end position
+    }
 }
 
-void layerSpecific(vector<vector<TH1*>> hs, vector<const char*> hnames, TCanvas *can)
+
+//Layers only histograms with names listed in vector-hnames
+void layerSpecific(vector<vector<TH1*>> hs, vector<const char*> hnames,vector<TString> inames, TCanvas *can)
 {
     int numF = hs.size();
     int numH = hs[0].size();
@@ -544,17 +525,78 @@ void layerSpecific(vector<vector<TH1*>> hs, vector<const char*> hnames, TCanvas 
 		cout << "Found "<< hnames[n] <<endl;
 		for (int j=0;j<numF;j++)
 		    plot_hists.push_back( hs[j][i] );
-		layerHistogram(plot_hists, can, n+1);
+		layerHistogram(plot_hists,inames,can,n+1);
 	    }
 	}
     }
     if (complete==hnames.size())
     {
     	can->Write();
-    	// gROOT->ProcessLine(".!mkdir -p ./output/"+outday+"/"+outname+"/");
-    	// crc->Print( "output/"+outday+"/"+outname+"/"+"crc_check.pdf","pdf");
-    	// crc->Print( "output/"+outday+"/"+outname+"/"+"crc_check.root","root");
     }
     else
     	cout << "Unable to locate all desired Histograms." << endl;
+}
+
+//Creates canvases containing all histograms for each file
+void plotAll(vector<vector<TH1*>> hs, vector<TString> inames, TString opath)
+{
+    cout << "Plotting all histograms." << endl;
+    int numF = hs.size();
+    int numH = hs[0].size();
+    int c_side = ceil(sqrt(numH));
+    for (int i=0;i<numF;i++)
+    {
+	TString name = "All_Histograms-";
+	name+=inames[i];
+	TCanvas* allc = newCanvas(name);
+	allc->SetName(name);
+	allc->SetTitle(name);
+	double ipart;
+	if(modf(sqrt(numH),&ipart) < 0.4)
+	    allc->Divide(c_side,c_side-1);
+	else
+	    allc->Divide(c_side,c_side);
+	for (int j=0;j<numH;j++)
+	{
+	    allc->cd(j+1);
+	    hs[i][j]->SetMarkerColor(kBlue+3);
+	    hs[i][j]->SetMarkerStyle(1);
+	    hs[i][j]->SetFillStyle(1001);
+	    hs[i][j]->SetFillColor(kBlue+3);
+	    hs[i][j]->SetLineColor(kBlue+3);
+	    gStyle->SetOptStat(111111);
+ 	    hs[i][j]->DrawCopy();
+	}
+	allc->Write();
+	
+	//Print in different formats
+	gROOT->ProcessLine(".!mkdir -p "+opath+"/individual_pdfs/");
+	allc->Print(opath+"individual_pdfs/"+inames[i]+".pdf","pdf");
+	gROOT->ProcessLine(".!mkdir -p "+opath+"/individual_pngs/");
+	allc->Print(opath+"individual_pngs/"+inames[i]+".png","png");
+	gROOT->ProcessLine(".!mkdir -p "+opath+"/individual_jpgs/");
+	allc->Print(opath+"individual_jpgs/"+inames[i]+".jpg","jpg");
+	gROOT->ProcessLine(".!mkdir -p "+opath+"/individual_roots/");
+	allc->Print(opath+"individual_roots/"+inames[i]+".root","root");
+    }
+}
+
+
+void layerAll(vector<vector<TH1*>> hs, vector<TString> inames)
+{
+    cout << "Layering all histograms." << endl;
+    int numF = hs.size();
+    int numH = hs[0].size();
+    vector<const char*> names;
+    for (int i=0;i<numH;i++)
+	names.push_back(hs[0][i]->GetName());
+    TCanvas* layc = newCanvas("All Histograms Layered");
+    int c_side = ceil(sqrt(numH));
+    double ipart;
+    if(modf(sqrt(numH),&ipart) < 0.4)
+	layc->Divide(c_side,c_side-1);
+    else
+	layc->Divide(c_side,c_side);
+    layc->SetName("AllHistogramsLayered");
+    layerSpecific(hs, names, inames, layc);
 }
