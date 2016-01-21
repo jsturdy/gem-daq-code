e0a8c7d7156995e6bcbc295e14834b24da78f259
Mykhailo Dalchenko <mykhailo.dalchenko@cern.ch>
2016-01-21 13:33:00 -0600

    adding dqm_checker base class

---
+++ gem-light-dqm/dqm-root/src/common/dqm_checker.cxx
@@ -0,0 +1,32 @@
+struct ErrorDetails
+{
+  long int eventNumber;
+  unsigned int[] errorCode;
+  std::string[] amcID;
+  std::string[] chamberID;
+  std::string[] chipID;
+  std::string[] errorMessage;
+};
+
+class GEMDQMchecker
+{
+  public:
+    GEMDQMchecker()
+    {
+      m_error = {-1,0,"N/A","N/A","N/A","N/A"};
+      m_eventStatus = true;
+      m_checkPerformed = false;
+    }
+    virtual ~GEMDQMchecker(){}
+    bool getEventStatus(Event *e)
+    {
+      if (m_checkPerformed) return m_eventStatus;
+      else throw std::logic_error("Event status check should be completed before calling this method");
+    }
+    void setCheckStatus(bool checkPerformed){m_checkPerformed = checkPerformed;}
+    ErrorDetails getError(){return m_error;}
+  private:
+    bool m_eventStatus;
+    bool m_checkPerformed;
+    ErrorDetails m_error;
+};
