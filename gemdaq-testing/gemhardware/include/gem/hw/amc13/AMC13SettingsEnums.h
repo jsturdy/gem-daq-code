403830e93b007acfeb9c4d8d1c8e1e5c884c1bf1
Jared Sturdy <sturdy@cern.ch>
2016-04-17 13:09:30 +0200

    added two enums, one for each TTC decoding style

--- gemdaq-testing/gemhardware/include/gem/hw/amc13/AMC13SettingsEnums.h
+++ gemdaq-testing/gemhardware/include/gem/hw/amc13/AMC13SettingsEnums.h
@@ -15,6 +15,16 @@ namespace gem {
             Resync    = 0x15, //Resync
             HardReset = 0x16, //HardReset
           } BGOCmd;
+
+        struct CSCGEMBGOCmd { //defines the CSCGEM format BGO commands
+          enum CSCGEMEBGOCmd { //defines the CSCGEM format BGO commands
+            BC0 = 0x1, //Bunch count reset 
+            EC0 = 0x2, //Event count reset
+            OC0 = 0x1, //Orbit count reset
+            CalPulse  = 0x14, //CalPulse
+            Resync    = 0x15, //Resync
+            HardReset = 0x16, //HardReset
+          } BGOCmd;
         };
 
       }; //end class AMC13Settings
@@ -24,7 +34,8 @@ namespace gem {
   }//end namespace gem::hw
   
   //typedef the enum for casting and access
-  typedef gem::hw::amc13::AMC13Settings::BGOCmd::EBGOCmd AMC13BGOCmd;
+  typedef gem::hw::amc13::AMC13Settings::BGOCmd::EBGOCmd             AMC13BGOCmd;
+  typedef gem::hw::amc13::AMC13Settings::CSCGEMBGOCmd::CSCGEMEBGOCmd GEMBGOCmd;
 }//end namespace gem
 
 #endif
